8a9ed4c304f5c60056e63463026468802d2b7192
QaisarTanvir <qaisar.tanvir@outlook.com>
2022-10-13 20:19:57 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ import sys
 
 path = "/opt/code/nndet"
 site_path = "/opt/dataiku/code-env/lib/python3.9/site-packages"
-with open(os.path.join(site_path,"easy-install.pth"), 'w') as f:
-    f.write(path)
-    
-with open(os.path.join(site_path,"nndet.egg-link"), 'w') as f:
-    f.write(path)
+
+try:
+    with open(os.path.join(site_path,"easy-install.pth"), 'w') as f:
+        f.write(path)
+
+    with open(os.path.join(site_path,"nndet.egg-link"), 'w') as f:
+        f.write(path)
+except:
+    pass
